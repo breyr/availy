977ab8cf92ca37c72d023f4f6aac5e1692c5007e
AdamSpera <adamspera@hotmail.com>
2022-01-22 17:26:34 -0500

    LC Boiler

--- app.js
+++ app.js
@@ -26,6 +26,79 @@ connection.connect(function (error) {
 })
 
 app.get('/', function (req, res) {
-    console.log(`[/]: Get request recieved at '/'`);
+    console.log(`[/]: Get request received at '/'`);
     res.sendFile('public/start.html', { root: __dirname });
+})
+
+app.post('/login', (req, res) => {
+    let body = ''; req.on('data', function (chunk) { body += chunk; });
+    req.on('end', function () {
+        console.log(`[/login]: Post request received at '/login' (${body})`);
+        // if data is received
+        if (body) {
+            // body[0] = username & body[1] = password
+            body = body.split(':');
+            // queries for account with the username of body[0] and password of body[1]
+            connection.query(`SELECT * FROM ${process.env.DATABASE}.userinfo WHERE username = ? AND password = ?`, [body[0], body[1]], function (error, results, fields) {
+                // if users defined exists
+                if (results.length > 0) {
+                    console.log(`Account Found: U:'${results[0].username}' P:'${results[0].password}'`);
+                    res.send('Login Successful');
+                } else {
+                    console.log('No Account Found');
+                    res.send('Incorrect Username and/or Password!');
+                }
+            });
+        } else {
+            // if data is not received
+            res.send('Body data invalid');
+        }
+    })
+})
+
+app.post('/create', (req, res) => {
+    let body = ''; req.on('data', function (chunk) { body += chunk; });
+    req.on('end', function () {
+        console.log(`[/create]: Post request received at '/create' (${body})`);
+        // if data is received
+        if (body) {
+            // body[0] = username & body[1] = email & body[2] = password & body[3] = employer
+            body = body.split(':');
+            // queries for account with the username of body[0]
+            connection.query(`SELECT * FROM ${process.env.DATABASE}.userinfo WHERE username = ?`, [body[0]], function (error, results, fields) {
+                if (results.length > 0) {
+                    console.log(`[/create]: Username already taken with the U:${body[0]}`);
+                    res.send('Username Already Taken')
+                } else {
+                    // queries for account with the email of body[1]
+                    connection.query(`SELECT * FROM ${process.env.DATABASE}.userinfo WHERE email = ?`, [body[1]], function (error, results, fields) {
+                        if (results.length > 0) {
+                            console.log(`[/create]: Email already taken with the E:${body[1]}`);
+                            res.send('Email Already Taken')
+                        } else {
+                            // verifies body[3] with env values and replaces with string
+                            if (body[3] == process.env.ARCADIAIT) {
+                                body[3] = 'ArcadiaIT';
+                                // sets body[0], body[1], body[2], and body[3] into a userinfo
+                                connection.query(`INSERT INTO ${process.env.DATABASE}.userinfo (username, email, password, employer) VALUES ('${body[0]}', '${body[1]}', '${body[2]}', '${body[3]}');`, function (error, result, field) {
+                                    // if insert executes unsuccessfully
+                                    if (!!error) {
+                                        console.log('[/create]: There was an issue creating account');
+                                        console.log(error);
+                                        res.send('Sorry! There was an issue creating your account. Try reloading.');
+                                    } else {
+                                        console.log('[/create]: Success creating account!');
+                                        res.send('Create Successful');
+                                    }
+                                }); // create account
+                            } else { res.send('Invalid Employer Code'); }
+                        }
+                    }); // email detection
+                }
+            }); // username detection
+        } else {
+            // if data is not received
+            res.send('Body data invalid');
+        }
+    })
 })
---
+++ public/javascript/start.js
@@ -0,0 +1,59 @@
+var optionsDiv = document.getElementById('optionsDiv');
+var loginBtn = document.getElementById('loginBtn');
+var createBtn = document.getElementById('createBtn');
+
+var loginDiv = document.getElementById('loginDiv');
+var loginUsername = document.getElementById('loginUsername');
+var loginPassword = document.getElementById('loginPassword');
+var loginDisplayText = document.getElementById('loginDisplayText');
+var loginSubmit = document.getElementById('loginSubmit');
+
+var createDiv = document.getElementById('createDiv');
+var createUsername = document.getElementById('createUsername');
+var createEmail = document.getElementById('createEmail');
+var createPassword = document.getElementById('createPassword');
+var employerCode = document.getElementById('employerCode');
+var createDisplayText = document.getElementById('createDisplayText');
+var createSubmit = document.getElementById('createSubmit');
+
+loginBtn.addEventListener('click', function () {
+    loginDiv.style.display = "block";
+    optionsDiv.style.display = "none";
+});
+createBtn.addEventListener('click', function () {
+    createDiv.style.display = "block";
+    optionsDiv.style.display = "none";
+});
+
+loginSubmit.addEventListener('click', function () {
+    if (loginUsername.value && loginPassword.value) {
+        fetch('/login', { method: 'POST', body: loginUsername.value + ':' + loginPassword.value })
+            .then(response => response.text())
+            .then(text => {
+                if (text == "Login Successful") {
+                    location.href = '/dashboard'
+                } else {
+                    loginDisplayText.innerText = text;
+                }
+            })
+    } else {
+        loginDisplayText.innerText = 'Login fields invalid.'
+    }
+});
+
+createSubmit.addEventListener('click', function () {
+    if (createUsername.value && createEmail.value && createPassword.value && employerCode.value) {
+        let data = `${createUsername.value}:${createEmail.value}:${createPassword.value}:${employerCode.value}`
+        fetch('/create', { method: 'POST', body: data })
+            .then(response => response.text())
+            .then(text => {
+                if (text == "Create Successful") {
+                    location.href = '/dashboard'
+                } else {
+                    createDisplayText.innerText = text;
+                }
+            })
+    } else {
+        createDisplayText.innerText = 'Create fields invalid.'
+    }
+});
